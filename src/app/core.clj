5362440944495b537b53aaa26947473bcba95e40
Matthew Davidson <matthew@modulolotus.net>
2017-04-07 19:30:04 -0400

    set-1-challenge-3

--- src/app/core.clj
+++ src/app/core.clj
@@ -2,7 +2,7 @@
   (:require [app.util :refer :all]
             [clojure.spec :as s]
             [clojure.string :refer [upper-case lower-case]]
-            [medley.core :refer [map-vals]]))
+            [medley.core :refer [filter-keys map-vals map-keys]]))
 
 (s/def ::probability (s/and number? #(<= 0 %) #(<= % 1)))
 (s/def ::hist (s/map-of char? number?))
@@ -19,10 +19,17 @@
 (defn letter-frequency [hist]
   ;;; Lowercases all letters and removes non-letters
   {:pre [(s/valid? ::hist hist)]}
-  (filter-keys Character/isLetter hist))
+  (let [nonletter-hist (filter-keys #(not (Character/isLetter %)) hist)
+        uppercase-hist (filter-keys #(Character/isUpperCase %) hist)
+        lowercase-hist (filter-keys #(Character/isLowerCase %) hist)]
+    (merge-with +
+                nonletter-hist
+                lowercase-hist
+                (map-keys #(Character/toLowerCase %) uppercase-hist))))
+
 
 (def en-letter-probabilities
-  (normalized-frequencies {\e 12.02 \t 9.10 \a 8.12 \o 7.68 \i 7.31 \n 6.95 \s 6.28 \r 6.02 \h 5.92 \d 4.32 \l 3.98 \u 2.88 \c 2.71 \m 2.61 \f 2.30 \y 2.11 \w 2.09 \g 2.03 \p 1.82 \b 1.49 \v 1.11 \k 0.69 \x 0.17 \q 0.11 \j 0.10 \z 0.07}))
+  (normalized-frequencies {\space 18.29 \e 10.27 \t 7.51 \a 6.53 \o 6.16 \i 5.67 \n 5.71 \s 5.32 \r 4.99 \h 4.98 \l 3.32 \d 3.28 \u 2.28 \c 2.23 \m 2.03 \f 1.98 \w 1.07 \g 1.62 \p 1.50 \y 1.43 \b 1.26 \v 0.80 \k 0.56 \x 0.14 \j 0.10 \q 0.08 \z 0.05 \. 1.39 \, 1.35 \" 0.68 \- 0.48 \' 0.36 \? 0.14 \! 0.10 \; 0.09 \: 0.06}))
 
 (defn chi2-for-letter [c num-c num-total-chars]
   {:pre [(s/valid? char? c) (s/valid? number? num-c)]}
@@ -43,14 +50,18 @@
   (let [key-bytes (byte-array (count bs) b)]
     (xor bs key-bytes)))
 
+(defn chi2-results [cipher-bytes bytes-to-xor]
+  (for [c bytes-to-xor]
+    (let [res (xor-with-byte-fill cipher-bytes (byte c))
+          res-string (byte-array->string res)
+          hist (letter-frequency (frequencies res-string))]
+      [res-string (chi2 hist) c])))
+
 ;;; Set 1, challenge 3
 (let [alphabet "abcdefghijklmnopqrstuvwxyz"
       xor-chars (str alphabet (upper-case alphabet))
       cipher-text "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
       cipher-bytes (hex->byte-array cipher-text)]
-  (for [c xor-chars]
-    (let [res (xor-with-byte-fill cipher-bytes (byte c))
-          res-string (byte-array->string res)
-          hist (frequencies res-string)]
-      ;; (byte-array->string res)
-      [res-string (chi2 hist)])))
+  (->> (chi2-results cipher-bytes xor-chars)
+       (sort-by second)
+       (first)))
