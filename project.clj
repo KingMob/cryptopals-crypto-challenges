0059df38217d5b3effa2da8d1d06c54576ff85eb
Matthew Davidson <matthew@modulolotus.net>
2017-05-19 16:50:35 -0400

    Fixed unbalanced bracket error in project.clj

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [commons-codec "1.10"]
                  [com.taoensso/tufte "1.1.1"]
                  [medley "1.0.0"]
-                 [byte-streams "0.2.2"]]
+                 [byte-streams "0.2.2"]
                  [org.apache.commons/commons-math3 "3.6.1"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]
                                   [alembic "0.3.2"]]}}
